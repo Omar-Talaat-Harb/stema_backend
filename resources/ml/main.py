375bed4fb5f83ffe20ac23ff5f50b1fd640dbd0d
Omar-Talaat-Harb <omartalaatharb@gmail.com>
2023-05-06 04:30:03 +0300

    machine model

---
+++ resources/ml/main.py
@@ -0,0 +1,28 @@
+from flask import *
+from predict import predict__random_forest
+
+app = Flask(__name__)
+@app.route('/success', methods = ['POST'])  #app url
+def success():
+  if request.method == 'POST':
+    quality_of_collection_bags_cleanness = request.form.get('quality_of_collection_bags_cleanness')
+    num_of_needles = request.form.get('num_of_needles')
+    satellite_bag = request.form.get('satellite_bag')
+    clots = request.form.get('clots')
+    net_weight = request.form.get('net_weight')
+    volume = request.form.get('volume')
+    prewbcsx109l = request.form.get('prewbcsx109l')
+    pre_cd_45_34 = request.form.get('pre_cd_45_34')
+    pre_viability_of_tnc = request.form.get('pre_viability_of_tnc')
+    post_wbcsx109_l = request.form.get('post_wbcsx109_l')
+
+    return predict__random_forest(quality_of_collection_bags_cleanness,num_of_needles,satellite_bag,clots,net_weight,volume,prewbcsx109l,pre_cd_45_34,pre_viability_of_tnc,post_wbcsx109_l)
+    
+
+
+
+
+if __name__ == "__main__":
+  app.run(host='127.0.0.1', debug=True)
+
+
---
+++ routes/web.php
@@ -0,0 +1,45 @@
+<?php
+
+use App\Http\Controllers\Doctor\ProfileController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\IndexController;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Admin\UserController;
+use App\Http\Controllers\Doctor\DashboardController;
+use App\Http\Controllers\Doctor\laboratoryController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+Route::get('/',[IndexController::class,'index'])->name('index');
+
+Route::get('/login',[LoginController::class,'login'])->name('login');
+Route::post('/login',[LoginController::class,'loginAccess'])->name('login');
+
+Route::resource('/admin/user',UserController::class);
+
+Route::get('/services',[DashboardController::class,'services'])->name('services');
+Route::get('/pending',[DashboardController::class,'pending'])->name('pending');
+
+Route::get('/laboratory1',[laboratoryController::class,'laboratory1'])->name('laboratory1');
+Route::post('/postlaboratory1',[laboratoryController::class,'postlaboratory1'])->name('postlaboratory1');
+Route::get('/laboratory2',[laboratoryController::class,'laboratory2'])->name('laboratory2');
+Route::post('/postlaboratory2',[laboratoryController::class,'postlaboratory2'])->name('postlaboratory2');
+Route::get('/laboratory3',[laboratoryController::class,'laboratory3'])->name('laboratory3');
+Route::post('/postlaboratory3',[laboratoryController::class,'postlaboratory3'])->name('postlaboratory3');
+Route::get('/laboratory4',[laboratoryController::class,'laboratory4'])->name('laboratory4');
+Route::post('/postlaboratory4',[laboratoryController::class,'postlaboratory4'])->name('postlaboratory4');
+
+Route::get('/disabledregstration',[DashboardController::class,'disabledregstration'])->name('disabledregstration');
+Route::get('/disabledcbu',[DashboardController::class,'disabledcbu'])->name('disabledcbu');
+Route::get('/disabledcollection',[DashboardController::class,'disabledcollection'])->name('disabledcollection');
+Route::get('cell',[DashboardController::class,'cell']);
+
+Route::get('/profile',[ProfileController::class,'showProfile'])->name('showProfile');
+Route::post('/profile-update',[ProfileController::class,'updateProfile'])->name('updateProfile');
